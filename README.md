5f4fa59fd4d516c89b8c61226f23b1704ba0ba2b
majid noureddine <nmajid@sqli.com>
2021-10-02 23:02:51 +0100

    export data as csv file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Sources de données
 
 - [wikipedia: Pandémie de Covid 19 au Maroc](https://fr.wikipedia.org/wiki/Pand%C3%A9mie_de_Covid-19_au_Maroc#D%C3%A9veloppement_du_virus)
+https://www.ionos.fr/digitalguide/sites-internet/developpement-web/web-scraping-avec-python/
--- src/crawlers/wikipedia.py
+++ src/crawlers/wikipedia.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
-import json
 import os
+import csv
 
 url = "https://fr.wikipedia.org/wiki/Pand%C3%A9mie_de_Covid-19_au_Maroc#D%C3%A9veloppement_du_virus"
 export_dir = "export/wikipedia"
-export_file_name = "data.json"
+export_file_name = "data.csv"
+file_header = ["date", "total_tests", "negative_tests", "positive_tests", "total_deaths", "reestablished_cases", "new_cases"]
 
 if not os.path.exists(export_dir):
     os.makedirs(export_dir)
@@ -28,25 +29,26 @@ def format_data(element):
         return 'error format'
 
 
-def get_columns(elements: list):
-    return {
-        "total_tests": elements[0],
-        "negatives": elements[1],
-        "positives": elements[2],
-        "deaths": elements[3],
-        "reestablishes": elements[4],
-        "new_cases": elements[5],
-    }
+def prepare_list(items: dict):
+    result = []
+    result.insert(0, items["date"])
+    return result + items["data"]
 
 
 formatted_data = []
 for index, inf in enumerate(items):
     if index == 9:
-        formatted_data.append({"date": format_data(inf), "data": get_columns(format_data(items[index + 1: index + 7]))})
+        formatted_data.append(
+            prepare_list({"date": format_data(inf), "data": format_data(items[index + 1: index + 7])})
+        )
     elif inf.startswith("\n") and index > 9:
-        formatted_data.append({"date": format_data(inf), "data": get_columns(format_data(items[index + 1: index + 7]))})
+        formatted_data.append(
+            prepare_list({"date": format_data(inf), "data": format_data(items[index + 1: index + 7])})
+        )
     else:
         continue
 
-with open(export_dir+"/"+export_file_name, "w") as f:
-    json.dump(formatted_data, f)
+with open(export_dir+"/"+export_file_name, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(file_header)
+    writer.writerows(formatted_data)
